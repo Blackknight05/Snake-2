6e84db9e920f7b6732363a677ae46ab8227bbf8c
Blackknight05 <149324803+Blackknight05@users.noreply.github.com>
2024-02-15 20:07:43 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,12 +1,10 @@
 import turtle
 import time
 import random
-from flask import Flask, render_template
 
-app = Flask(__name__)
-
-# Game logic
 delay = 0.1
+
+# Score
 score = 0
 high_score = 0
 
@@ -81,33 +79,104 @@ def move():
         head.setx(x + 20)
 
 # Keyboard bindings
-@app.route('/')
-def home():
-    return render_template('index.html')
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
 
-@app.route('/move_up')
-def move_up():
-    go_up()
-    move()
-    return 'Success'
+# Main game loop
+while True:
+    wn.update()
 
-@app.route('/move_down')
-def move_down():
-    go_down()
-    move()
-    return 'Success'
+    # Check for a collision with the border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
 
-@app.route('/move_left')
-def move_left():
-    go_left()
-    move()
-    return 'Success'
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+        
+        # Clear the segments list
+        segments.clear()
 
-@app.route('/move_right')
-def move_right():
-    go_right()
-    move()
-    return 'Success'
+        # Reset the score
+        score = 0
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        # Reset the delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+
+    # Check for a collision with the food
+    if head.distance(food) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Shorten the delay
+        delay -= 0.001
+
+        # Increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()    
+
+    # Check for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+        
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+        
+            # Clear the segments list
+            segments.clear()
+
+            # Reset the score
+            score = 0
+
+            # Reset the delay
+            delay = 0.1
+        
+            # Update the score display
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    time.sleep(delay)
+
+wn.mainloop()
